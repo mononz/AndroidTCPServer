ae049b5d959fcfc7efbb50ad1ab22d04fa3f1ebd
Jared Hall <jaredrexhall@gmail.com>
2013-06-27 21:38:27 +1200

    created repo and added project files

---
+++ src/Constants.java
@@ -0,0 +1,12 @@
+/**
+ * Description
+ *
+ * @author Catalin Prata
+ * Date: 2/12/13
+ */
+public class Constants {
+
+	public static final String CLOSED_CONNECTION = "client_closed_connection";
+	public static final String LOGIN_NAME = "client_login_name";
+
+}
